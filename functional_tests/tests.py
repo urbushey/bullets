83281683438589fba8ac14cb93f84e57572d8f13
Uri Bushey <ubushey@appnexus.com>
2014-10-12 11:11:46 -0400

    Updated functional tests.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,6 +38,9 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Milton hits enter, and now the new bullet appears in the table.
         inputbox.send_keys(Keys.ENTER)
+        milton_bullets_url = self.browser.current_url
+        self.assertRegex(milton_bullets_url,
+                         '/bullets/.+')
 
         # Milton wants to give Bereket some credit too by entering a second
         # item.
@@ -48,11 +51,40 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_list_table_for_row('+ Uri saved the day.')
         self.check_list_table_for_row('+ Bereket committed some sick code too.')
 
+        # Bereket wants to enter bullets also
+        ## New web browser session
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Bereket visits home page. Does not see anyone else's bullets
+        ## This is because I am following along with the TDD python book
+        ## Normally we want him to see other people's bullets
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('Uri saved the day', page_text)
+        self.assertNotIn('Bereket committed some sick code', page_text)
+
+        # Bereket enters his own bullet
+        inputbox = self.browser.find_element_by_id('id_new_bullet')
+        inputbox.send_keys('Had a bad day')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Bereket gets his own URL
+        bereket_bullets_url = self.browser.current_url
+        self.assertRegex(bereket_bullets_url,
+                         '/lists/.+')
+        self.assertNotEqual(bereket_bullets_url, milton_bullets_url)
+
+        # Again, make sure Bereket doesn't see Milton's bullets
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('Uri saved the day', page_text)
+        self.assertIn('Had a bad day', page_text)
+
+        self.fail("Finish writing the test!")
+
         # Milton can choose whether this bullet is + or -
         # Milton can enter the bullet and press enter and it is saved
         # Milton can come back to the site and see his bullets
-        self.fail("Finish writing the test!")
-
     # Bereket can come to the site and see Milton's bullet
     # Bereket can post his own bullet
     # Bereket can see Milton's bullet and his own bullet on the site
