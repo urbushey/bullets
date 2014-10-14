f4b0e07731df4840154217465117feb31360fbec
Uri Bushey <ubushey@appnexus.com>
2014-10-14 00:42:31 -0400

    Separated out urls for overall app and bullet_app

---
+++ bullet_app/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'bullets.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'bullet_app.views.home_page', name='home'),
+    url(r'^(\d+)/$', 'bullet_app.views.view_bullets', name='view_bullets'),
+    url(r'^(\d+)/add_bullet$', 'bullet_app.views.add_bullet', name='add_bullet'),
+    url(r'^new$', 'bullet_app.views.new_bullet',name='new_bullet'),
+
+)
