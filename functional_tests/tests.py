d32eb30ff520dffc7c3a442a227d98a77c9f1255
Uri Bushey <ubushey@appnexus.com>
2014-11-29 09:16:38 -0700

    Test for styling and layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -92,6 +92,29 @@ class NewVisitorTest(LiveServerTestCase):
                 option.click()
                 break
 
+    def test_layout_and_styling(self):
+        # Bereket goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # she notices the input box is neatly centered
+        inputbox = self.browser.find_element_by_id('id_new_bullet')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=25)  # I am not following the book exactly but I want
+                       # this test to pass even with the +/- dropdown
+
+        # she sees the same thing when she submits input
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_bullet')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=25)  # I am not following the book exactly but I want
+                       # this test to pass even with the +/- dropdown
+
+
         # Retrieve the bullet and make sure it has a negative value
 
 
