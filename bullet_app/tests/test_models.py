c4bf6d8cc6abb855a5d925e25f74b4a502641dc5
Uri Bushey <ubushey@appnexus.com>
2015-01-11 23:16:53 -0500

    Moved unit tests in bullet_app into their own files.

---
+++ bullet_app/tests/test_models.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from bullet_app.models import Bullet, BulletGroup
+
+# Create your tests here.
+
+
+class BulletAndBulletGroupModelsTest(TestCase):
+    def test_saving_and_retrieving_bullets(self):
+        bullet_group_ = BulletGroup()
+        bullet_group_.save()
+
+        first_bullet = Bullet()
+        first_bullet.text = 'The first (ever) bullet. Bang!'
+        first_bullet.sign = '+'
+        first_bullet.bullet_group = bullet_group_
+        first_bullet.save()
+
+        second_bullet = Bullet()
+        second_bullet.text = 'Second bullet'
+        second_bullet.sign = '-'
+        second_bullet.bullet_group = bullet_group_
+        second_bullet.save()
+
+        saved_group = BulletGroup.objects.first()
+        self.assertEqual(saved_group, bullet_group_)
+
+        saved_bullets = Bullet.objects.all()
+        self.assertEqual(saved_bullets.count(), 2)
+
+        first_saved_bullet = saved_bullets[0]
+        second_saved_bullet = saved_bullets[1]
+        self.assertEqual(first_saved_bullet.text,
+                         'The first (ever) bullet. Bang!')
+        self.assertEqual(first_saved_bullet.sign,
+                         '+')
+        self.assertEqual(first_saved_bullet.bullet_group, bullet_group_)
+        self.assertEqual(second_saved_bullet.text,
+                         'Second bullet')
+        self.assertEqual(second_saved_bullet.sign,
+                         '-')
+        self.assertEqual(second_saved_bullet.bullet_group, bullet_group_)
