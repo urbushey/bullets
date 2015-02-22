0917bb484ef02acca88128192a252de758be8a62
Uri Bushey <ubushey@appnexus.com>
2015-02-22 17:26:43 -0500

    Restricting scope of bullet parsing to one positive or negative (signed bullets) for now

--- functional_tests/test_bullet_parsing.py
+++ functional_tests/test_bullet_parsing.py
@@ -21,49 +21,49 @@ class BulletParsingTest(FunctionalTest):
         self.assertIn('This is bad', row.text)
         self.assertEqual(row.positive_score, 0)
         self.assertEqual(row.negative_score, 1)
-        # Uri wants to add a bullet that has 3 positives.
-        self.browser.find_element_by_id('id_new_bullet').send_keys(
-                                        '+++ Triple good!\n')
-        row = self.get_bullets_table()[2]
-        self.assertIn('Triple good!', row.text)
-        self.assertEqual(row.positive_score, 3)
-        self.assertEqual(row.negative_score, 0)
-        # Uri wants to add a bullet that has 3 negatives.
-        self.browser.find_element_by_id('id_new_bullet').send_keys(
-                                        '+++ Triple bad!\n')
-        row = self.get_bullets_table()[3]
-        self.assertIn('Triple bad!', row.text)
-        self.assertEqual(row.positive_score, 0)
-        self.assertEqual(row.negative_score, 3)
-        # Uri wants to add a bullet that has 1 positive and one negative.
-        # Note that the '-' in the middle of the sentence is not
-        # incorreclty counted.
-        self.browser.find_element_by_id('id_new_bullet').send_keys(
-                                        '+/- One good - one bad!\n')
-        row = self.get_bullets_table()[4]
-        self.assertIn('One good - one bad!', row.text)
-        self.assertEqual(row.positive_score, 1)
-        self.assertEqual(row.negative_score, 1)
-        # Uri wants to add a bullet that has 4 positives and 3 negatives.
-        self.browser.find_element_by_id('id_new_bullet').send_keys(
-                                        '++++--- Four good three bad!\n')
-        row = self.get_bullets_table()[5]
-        self.assertIn('Four good three bad!', row.text)
-        self.assertEqual(row.positive_score, 4)
-        self.assertEqual(row.negative_score, 3)
-        # Uri wants to add a bullet that has 5 positive and 5 negatives.
-        self.browser.find_element_by_id('id_new_bullet').send_keys(
-                                        '+++++ ----- Five good five bad!\n')
-        row = self.get_bullets_table()[6]
-        self.assertIn('Five good five bad!', row.text)
-        self.assertEqual(row.positive_score, 5)
-        self.assertEqual(row.negative_score, 5)
-        # Uri can't add a bullet with more than 5 positives or 5 -.
-        self.browser.find_element_by_id('id_new_bullet').send_keys(
-                                        '++++++++ --------- \
-                                        Still five good five bad!\n')
-        row = self.get_bullets_table()[7]
-        self.assertIn('Still five good five bad!', row.text)
-        self.assertEqual(row.positive_score, 5)
-        self.assertEqual(row.negative_score, 5)
+        # # Uri wants to add a bullet that has 3 positives.
+        # self.browser.find_element_by_id('id_new_bullet').send_keys(
+        #                                 '+++ Triple good!\n')
+        # row = self.get_bullets_table()[2]
+        # self.assertIn('Triple good!', row.text)
+        # self.assertEqual(row.positive_score, 3)
+        # self.assertEqual(row.negative_score, 0)
+        # # Uri wants to add a bullet that has 3 negatives.
+        # self.browser.find_element_by_id('id_new_bullet').send_keys(
+        #                                 '+++ Triple bad!\n')
+        # row = self.get_bullets_table()[3]
+        # self.assertIn('Triple bad!', row.text)
+        # self.assertEqual(row.positive_score, 0)
+        # self.assertEqual(row.negative_score, 3)
+        # # Uri wants to add a bullet that has 1 positive and one negative.
+        # # Note that the '-' in the middle of the sentence is not
+        # # incorreclty counted.
+        # self.browser.find_element_by_id('id_new_bullet').send_keys(
+        #                                 '+/- One good - one bad!\n')
+        # row = self.get_bullets_table()[4]
+        # self.assertIn('One good - one bad!', row.text)
+        # self.assertEqual(row.positive_score, 1)
+        # self.assertEqual(row.negative_score, 1)
+        # # Uri wants to add a bullet that has 4 positives and 3 negatives.
+        # self.browser.find_element_by_id('id_new_bullet').send_keys(
+        #                                 '++++--- Four good three bad!\n')
+        # row = self.get_bullets_table()[5]
+        # self.assertIn('Four good three bad!', row.text)
+        # self.assertEqual(row.positive_score, 4)
+        # self.assertEqual(row.negative_score, 3)
+        # # Uri wants to add a bullet that has 5 positive and 5 negatives.
+        # self.browser.find_element_by_id('id_new_bullet').send_keys(
+        #                                 '+++++ ----- Five good five bad!\n')
+        # row = self.get_bullets_table()[6]
+        # self.assertIn('Five good five bad!', row.text)
+        # self.assertEqual(row.positive_score, 5)
+        # self.assertEqual(row.negative_score, 5)
+        # # Uri can't add a bullet with more than 5 positives or 5 -.
+        # self.browser.find_element_by_id('id_new_bullet').send_keys(
+        #                                 '++++++++ --------- \
+        #                                 Still five good five bad!\n')
+        # row = self.get_bullets_table()[7]
+        # self.assertIn('Still five good five bad!', row.text)
+        # self.assertEqual(row.positive_score, 5)
+        # self.assertEqual(row.negative_score, 5)
 
